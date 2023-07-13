668ffa78b04901327de47225602b0c8f273d73d0
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-07-13 18:38:25 +0000

    updated the city and country dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 import "../style/index.css";
-
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
@@ -28,7 +27,7 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  let socialMediaIcons = ""; // Initialize an empty string for social media icons
+  let socialMediaIcons = "";
 
   if (variables.twitter !== null && variables.twitter !== "") {
     const twitterURL = `https://twitter.com/${variables.twitter}`;
@@ -50,61 +49,105 @@ function render(variables = {}) {
     socialMediaIcons += `<li><a href="${instagramURL}"><i class="fab fa-instagram"></i></a></li>`;
   }
 
-  // Update country dropdown options based on the selected city
   const countryDropdown = document.querySelector("#country");
-  const usaOption = countryDropdown.querySelector("option[value='USA']");
-  usaOption.style.display = variables.city === "Miami" ? "block" : "none";
+  const cityDropdown = document.querySelector("#city");
 
-  const germanyOption = countryDropdown.querySelector(
-    "option[value='Germany']"
-  );
-  germanyOption.style.display = variables.city === "Munich" ? "block" : "none";
-  const venezuelaOption = countryDropdown.querySelector(
-    "option[value='Venezuela']"
-  );
-  venezuelaOption.style.display =
-    variables.city === "Caracas" ? "block" : "none";
-  const canadaOption = countryDropdown.querySelector("option[value='Canada']");
-  canadaOption.style.display = variables.city === "Toronto" ? "block" : "none";
+  // Reset country dropdown to show all options.In the condition && option.value !-- "", This ensures that the "null" option remains visible in the country dropdown, allowing users to select it to see all the country options
+  countryDropdown.querySelectorAll("option").forEach(option => {
+    option.style.display = "block";
+  });
+
+  if (variables.city === "Miami") {
+    // Show only USA in country dropdown if Miami is selected
+    countryDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "USA" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.city === "Toronto") {
+    // Show only Canada in country dropdown if Toronto is selected
+    countryDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Canada" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.city === "Munich") {
+    // Show only Germany in country dropdown if Munich is selected
+    countryDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Germany" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.city === "Caracas") {
+    // Show only Venezuela in country dropdown if Caracas is selected
+    countryDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Venezuela" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  }
+
+  // Reset city dropdown to show all options . In the condition && option.value !-- "", This ensures that the "null" option remains visible in the city dropdown, allowing users to select it to see all the city options
+  cityDropdown.querySelectorAll("option").forEach(option => {
+    option.style.display = "block";
+  });
+
+  if (variables.country === "USA") {
+    // Show only Miami in city dropdown if USA is selected
+    cityDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Miami" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.country === "Canada") {
+    // Show only Toronto in city dropdown if Canada is selected
+    cityDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Toronto" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.country === "Germany") {
+    // Show only Munich in city dropdown if Germany is selected
+    cityDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Munich" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  } else if (variables.country === "Venezuela") {
+    // Show only Caracas in city dropdown if Venezuela is selected
+    cityDropdown.querySelectorAll("option").forEach(option => {
+      if (option.value !== "Caracas" && option.value !== "") {
+        option.style.display = "none";
+      }
+    });
+  }
 
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-            <img src="${variables.avatarURL}" class="photo" />
-            <h1>${
-              variables.name == null || variables.name == ""
-                ? `first`
-                : variables.name
-            } ${
+    ${cover}
+    <img src="${variables.avatarURL}" class="photo" />
+    <h1>${
+      variables.name == null || variables.name == "" ? "first" : variables.name
+    } ${
     variables.lastname == null || variables.lastname == ""
-      ? `last`
+      ? "last"
       : variables.lastname
   }</h1>
-        
-            <h2>${variables.role == null ? "Role" : variables.role}</h2>
-            <h3>${variables.city == null ? "City" : variables.city} ${
+    <h2>${variables.role == null ? "Role" : variables.role}</h2>
+    <h3>${variables.city == null ? "City" : variables.city} ${
     variables.country == null ? "Country" : variables.country
   }</h3>
-        
-            <ul class="${variables.socialMediaPosition}">
-              ${socialMediaIcons} <!-- Insert the dynamically generated social media icons -->
-            </ul>
-          </div>`;
+    <ul class="${variables.socialMediaPosition}">
+      ${socialMediaIcons}
+    </ul>
+  </div>`;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
@@ -115,12 +158,11 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); //render the card for the first time
+  render(window.variables);
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -130,7 +172,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+      render(Object.assign(window.variables, values));
     });
   });
 };
